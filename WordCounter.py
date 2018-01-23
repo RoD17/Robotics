a8a10f661c6c5d401c994ac92409b309dfb664c9
Rodney <Rodney.g.greene17@gmail.com>
2018-01-22 20:52:16 -0500

    WordCounter project

---
+++ WordCounter.py
@@ -0,0 +1,16 @@
+def count(sequence, item):
+cont=0
+for i in sequence:
+    if item==i:
+        cont+=1
+return cont
+
+def interate(string):
+	if(string != null)
+		word = string.split(maxsplit=1)
+		count count(string, word)
+		return print("'" + word + "': " + count ", ")
+
+string = raw_input("Enter a string:")
+
+interate(%s)
